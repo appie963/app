d22f8c5199e5b30ef565f59231e3677904d173ab
appie963 <appie963@live.com>
2020-09-18 18:02:10 +0800

    1

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, redirect, url_for, request, render_template
+import pymysql
 
 app = Flask(__name__)
+
+
+def sql(sql_req='select * from accout'):
+    # 连接database
+    conn = pymysql.connect('132.232.63.133', 'root', 'rootroot', 'accbook')
+    cursor = conn.cursor()
+    cursor.execute(sql_req)
+    res = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return res
+
+
+print()
+s = sql('select * from usertable')
 
 
 # 首页自动跳转至login.html
@@ -23,11 +39,24 @@ def success():
 # 登录认证界面
 @app.route('/login', methods=['POST'])
 def login():
-    login_name = 'admin'
-    login_passwd = 'admin123'
-    if request.method == 'POST' and request.form['nm'] == login_name and request.form['passwd'] == login_passwd:
-        user = request.form['nm']
-        return render_template('success.html', user=user)
+    if request.method == 'POST':
+        return render_template('success.html', user=request.form['nm'], passwd=request.form['passwd'])
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/_register')
+def _register():
+    return render_template("register.html")
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    # 判断是否为post请求
+    if request.method == 'POST':
+        print(request.form['reg_name'])
+        print(request.form['reg_passwd'])
+        return render_template('success.html', user=request.form['reg_name'], passwd=request.form['reg_passwd'])
     else:
         return redirect(url_for('index'))
 
